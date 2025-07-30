73aa63d74619c46d30235b3e258d7cf1798d88cd
Yasmin Ventura <yasmin.gomsv@gmail.com>
2025-07-30 15:11:03 -0300

    MethodArgumentNotValidException

--- library-system/src/main/java/com/example/library_system/exceptions/GlobalExceptionHandler.java
+++ library-system/src/main/java/com/example/library_system/exceptions/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ import com.example.library_system.exceptions.custom.EmailAlreadyExistsException;
 import com.example.library_system.exceptions.custom.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
@@ -35,5 +37,22 @@ public class GlobalExceptionHandler {
                 List.of(ex.getMessage())
         );
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> validationErros = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> String.format("Field '%s' %s", error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                "Validation failed",
+                HttpStatus.BAD_REQUEST.value(),
+                validationErros
+        );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
